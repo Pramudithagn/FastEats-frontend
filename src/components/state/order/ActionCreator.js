e9583ca02cefc999d08f193e2ac9693980867b42
Pramuditha <pramudithagn@gmail.com>
2024-07-15 02:17:09 +0530

    order actions

---
+++ src/components/state/order/ActionCreator.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './ActionType';
+
+export const createOrderRequest = () => ({
+  type: actionTypes.CREATE_ORDER_REQUEST,
+});
+
+export const createOrderSuccess = (order) => ({
+  type: actionTypes.CREATE_ORDER_SUCCESS,
+  payload: order,
+});
+
+export const createOrderFailure = (error) => ({
+  type: actionTypes.CREATE_ORDER_FAILURE,
+  payload: error,
+});
+
+export const getUsersOrdersRequest = () => ({
+    type: actionTypes.GET_USERS_ORDERS_REQUEST,
+  });
+  
+  export const getUsersOrdersSuccess = (orders) => ({
+    type: actionTypes.GET_USERS_ORDERS_SUCCESS,
+    payload: orders,
+  });
+  
+  export const getUsersOrdersFailure = (error) => ({
+    type: actionTypes.GET_USERS_ORDERS_FAILURE,
+    payload: error,
+  });
+  
