d3a014b1b221a6e28e50145d2a9bfd744a1634fc
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-08-04 08:46:41 +0530

    menu

---
+++ src/Admin/Menu/RestaurantsMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuTable from './MenuTable'
+
+function RestaurantsMenu() {
+  return (
+    <div className='px-2'>
+      <MenuTable name={"All Menu Items"}/>
+    </div>  
+    )
+}
+
+export default RestaurantsMenu
