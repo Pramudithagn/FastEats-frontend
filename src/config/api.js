b0c0a3c8b7e0e70b43de0ef43f28a14bf221012f
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-08-07 23:49:03 +0530

    env

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080"
+// export const API_URL = "http://localhost:8080"
+export const API_URL = process.env.REACT_APP_API_URL
 
 export const api = axios.create({
     baseURL: API_URL,
