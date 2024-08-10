fa05f9be14639563671581fb48d9e831209302fa
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-08-10 11:51:19 +0530

    settings change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import { NavBar } from './components/NavBar/NavBar';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { darkTheme } from './Theme/DarkTheme';
-import { Home } from './components/Home/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBar } from "./components/NavBar/NavBar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { darkTheme } from "./Theme/DarkTheme";
+import { Home } from "./components/Home/Home";
+import { RestaurantDetails } from "./components/Restaurant/RestaurantDetails";
+import { Cart } from "./components/Cart/Cart";
+import { CartItem } from "./components/Cart/CartItem";
+import { Profile } from "./components/Profile/Profile";
+import { CustomerRoutes } from "./Routes/CustomerRoutes";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./components/state/auth/Action";
+import { findCart } from "./components/state/cart/Action";
+import { Routers } from "./Routes/Routers";
+import { getRestaurantByUserId } from "./components/state/restaurant/Action";
 
 function App() {
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { auth } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch(getUser(auth.jwt || jwt));
+    dispatch(findCart(jwt));
+  }, [auth.jwt]);
+
+  useEffect(() => {
+    if (auth.user?.role == "ROLE_OWNER") {
+      dispatch(getRestaurantByUserId(auth.jwt || jwt));
+    }
+  }, [auth.user]);
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-    <NavBar/>
-    <Home/>
-
+      <CssBaseline />
+      {/* <NavBar/> */}
+      {/* <Home/> */}
+      {/* <RestaurantDetails/> */}
+      {/* <Cart/> */}
+      {/* <CartItem/> */}
+      {/* <Profile/> */}
+      {/* <CustomerRoutes /> */}
+      <Routers/>
     </ThemeProvider>
   );
 }
--- src/Theme/DarkTheme.js
+++ src/Theme/DarkTheme.js
@@ -4,18 +4,21 @@ export const darkTheme = createTheme({
     palette:{
         mode:"dark",
         primary:{
-            main:"#e91e63"
+            main:"#ff1744"
         },
         secondary:{
-            main:"#5A20CB"
+            main:"#b0b0b0"
         },
         black:{
             main:"#242B2E"
         },
+        blue:{
+            main:"#2196f3"
+        },
         background:{
             main:"#000000",
             default:"#0D0D0D",
-            paper:"#0D0D0D"
+            paper:"#262626"
         },
         textColor:{
             main:"#111111"
